7654cfa56c8b01a834e7c811fcd5a0036ab58a3a
lebmax <lebmaaax@gmail.com>
2025-04-23 11:58:27 +0300

    auth.py update

--- auth-service/app/auth.py
+++ auth-service/app/auth.py
@@ -1,11 +1,28 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends, HTTPException
+from sqlalchemy.orm import Session
+import models, schemas, utils
+from database import SessionLocal
 
 router = APIRouter()
 
-@router.post("/register")
-def register_user():
-    pass
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
 
-@router.post("/login")
-def login_user():
-    pass
+@router.post("/register", response_model=schemas.UserOut)
+def register_user(user: schemas.UserCreate, db: Session = Depends(get_db)):
+    db_user = db.query(models.User).filter(models.User.username == user.username).first()
+    if db_user:
+        raise HTTPException(status_code=400, detail="Username already registered")
+
+    hashed_password = utils.hash_password(user.password)
+    new_user = models.User(username=user.username, hashed_password=hashed_password)
+
+    db.add(new_user)
+    db.commit()
+    db.refresh(new_user)
+
+    return new_user
